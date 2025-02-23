a82c4956abebe6c6248f468d78ee20f1da57ef0c
Dimmis5 <zhangdimeo@gmail.com>
2025-02-23 16:40:24 +0100

    Boucle

--- Convertisseur/Convertisseur.py
+++ Convertisseur/Convertisseur.py
@@ -1,66 +1,71 @@
 import requests
 
-print("Convertisseur")
-print("1. Degres en celsus")
-print("2. Celsus en degres")
-print("3. Conversion Devise")
-print("4. Conversion de longueur m en km")
-print("5. Conversion de longueur de km en m")
-print("6. Conversion de poids gramme en kilogramme")
 
-Choix = input("Choisir une option :")
+while True:
+    print("Convertisseur")
+    print("1. Degres en celsus")
+    print("2. Celsus en degres")
+    print("3. Conversion Devise")
+    print("4. Conversion de longueur m en km")
+    print("5. Conversion de longueur de km en m")
+    print("6. Conversion de poids gramme en kilogramme")
+    print("100. Quitter le programme")
 
-if Choix =="1":
-    valeur = float(input("Rentrez la température en degres :"))
-    celsus = valeur + 273
-    print(celsus)
+    Choix = input("Choisir une option :")
 
-elif Choix =="2" :
-    valeur = float(input("Rentrez une température en Celsus :"))
-    degres = valeur - 273
-    print(degres)
+    if Choix =="1":
+        valeur = float(input("Rentrez la température en degres :"))
+        celsus = valeur + 273
+        print(celsus)
 
-elif Choix =="3":
+    elif Choix =="2" :
+        valeur = float(input("Rentrez une température en Celsus :"))
+        degres = valeur - 273
+        print(degres)
 
-    api_key = "73a15a97425e707ad2ceb2ca"
-    url = "https://v6.exchangerate-api.com/v6/73a15a97425e707ad2ceb2ca/latest/USD"
+    elif Choix =="3":
 
-    Devise_de_départ = input("Devise de départ : ").upper()
-    Devise_arriver = input("Devise cible: ").upper()
-    Montant = float(input("Montant à convertir : "))
+        api_key = "73a15a97425e707ad2ceb2ca"
+        url = "https://v6.exchangerate-api.com/v6/73a15a97425e707ad2ceb2ca/latest/USD"
 
-    reponse = requests.get(url)
+        Devise_de_départ = input("Devise de départ : ").upper()
+        Devise_arriver = input("Devise cible: ").upper()
+        Montant = float(input("Montant à convertir : "))
 
-    if reponse.status_code == 200 : 
-        data = reponse.json()
-        taux = data['conversion_rates'][Devise_arriver]
-        valeur = taux * Montant
-        print(f"\n {Montant} {Devise_de_départ} = {valeur:.2f} {Devise_arriver} (Taux: {taux})")
-    else:
-        print("Erreur lors de la récupération des données.")
+        reponse = requests.get(url)
 
-
-if Choix =="4":
-    valeur = float(input("Entrez une valeur en m :"))
-    resultat = valeur *0.001
-    print(resultat)
+        if reponse.status_code == 200 : 
+            data = reponse.json()
+            taux = data['conversion_rates'][Devise_arriver]
+            valeur = taux * Montant
+            print(f"\n {Montant} {Devise_de_départ} = {valeur:.2f} {Devise_arriver} (Taux: {taux})")
+        else:
+            print("Erreur lors de la récupération des données.")
 
 
-if Choix =="5":
-    valeur = float(input("Entrez une valeur en km :"))
-    resultat = valeur * 1000
-    print(resultat)
+    if Choix =="4":
+        valeur = float(input("Entrez une valeur en m :"))
+        resultat = valeur *0.001
+        print(resultat)
 
-if Choix == "6":
-    valeur = float(input("Entrez une valeur en gramme : "))
-    resultat = valeur * 0.001
-    print(resultat)
 
-if Choix == "7":
-    valeur = float(input("Entrez une valeur en kg :"))
-    resultat = valeur * 1000
-    print(resultat)
+    if Choix =="5":
+        valeur = float(input("Entrez une valeur en km :"))
+        resultat = valeur * 1000
+        print(resultat)
 
-    
-else :
-    print("Option invalide")    
+    if Choix == "6":
+        valeur = float(input("Entrez une valeur en gramme : "))
+        resultat = valeur * 0.001
+        print(resultat)
+
+    if Choix == "7":
+        valeur = float(input("Entrez une valeur en kg :"))
+        resultat = valeur * 1000
+        print(resultat)
+
+    if Choix == "100":
+        break
+
+    else :
+        print("Option invalide")    
